b4033a0063222124badea84b9076f8e755eb8493
LXMedia1 <anfrbo@gmail.com>
2014-10-27 19:04:13 +0100

    Revert "x"
    This reverts commit f727cb220ede4bb8bb763d761080c16afb278b2e.

--- LexxersAIOCarry/Program.cs
+++ LexxersAIOCarry/Program.cs
@@ -24,8 +24,7 @@ namespace UltimateCarry
 		private static void Game_OnGameLoad(EventArgs args)
 		{
 			//AutoUpdater.InitializeUpdater();
-			Chat.Print("Ultimate Carry 2 delited, say thx to imeh on IRC or Forum, he banned me");
-			return;
+			Chat.Print("Ultimate Carry Version " + LocalVersion + " load ...");
 			Helper = new Helper();
 
 			Menu = new Menu("UltimateCarry", "UltimateCarry_" + ObjectManager.Player.ChampionName, true);
